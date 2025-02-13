10500f6fa7e8791ed624733a49b8b0d03788324d
Scott Willeke <scott@willeke.com>
2025-02-13 10:24:55 -0800

    fix: update toplevel dir for commit hook when installed

--- scripts/git-hook-commit.sh
+++ scripts/git-hook-commit.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+# When running as a hook, we're in .git/hooks/, so need to go up two levels
+# When running directly, we're in scripts/, so need to go up one level
 this_dir=$(cd $(dirname "$0"); pwd)
-parent_dir=$(cd "$this_dir/.."; pwd)
-repo_dir=$(cd "$parent_dir"; pwd)
+repo_dir=$(git rev-parse --show-toplevel)
 
 # the goal here is just to make sure at minimum kubectl kustomize processes all the yaml
 check_kustomize() {
