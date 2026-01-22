43e7ffa63434c274dd43d65a221a38b89eda2d41
Scott Willeke <scott@willeke.com>
2026-01-21 20:13:55 -0800

    update the ramblefeed base resources (to use the right probes)

--- apps/base/ramblefeed/copy-from-source.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-this_dir=$(cd $(dirname "$0"); pwd) # this script's directory
-this_script=$(basename $0)
-this_script_base=$(basename "$0" .sh)
-
-cp ~/src/activescott/ramblefeed/k8s/base/*.yaml "$this_dir/"
